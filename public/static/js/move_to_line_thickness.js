43dbf798f69f0cb0913af96440df519301414ffa
Sam Szreter <s.szreter@gmail.com>
2017-04-13 16:00:01 +0100

    Add hover tooltip behaviour
    Added handlers so that tooltips displaying the specific moves shared
    by any two pokemon are displayed on hover.

--- public/static/js/move_to_line_thickness.js
+++ public/static/js/move_to_line_thickness.js
@@ -24,9 +24,9 @@ d3.json("static/data/force_data_151.json", function(error, graph) {
     .alpha(0.1)
     .nodes(filtered_nodes)
     .links(filtered_links)
-    .charge(-300)
-    .chargeDistance(500)
-    .linkDistance(image_size*0.6)
+    .charge(-200)
+    .chargeDistance(1000)
+    .linkDistance(image_size*0.6*2)
 //    .linkDistance(function (link) { return ( link.value )*6 })
     .on("tick", tick);
 
@@ -98,7 +98,9 @@ d3.json("static/data/force_data_151.json", function(error, graph) {
     links.enter()
       .append("line")
       .attr("class", "link")
-      .style("stroke-width", function(d) { return calculate_link_stroke_width(d) });
+      .style("stroke-width", function(d) { return calculate_link_stroke_width(d) })
+      .on('mousemove', (d) => handleMousemove(d))
+      .on('mouseout', (d) => removeTooltips());
 
     links.exit().remove();
 
@@ -122,7 +124,9 @@ d3.json("static/data/force_data_151.json", function(error, graph) {
       .attr("xlink:href", function(d) { return "sprites/pokeapi_cache_sprites_gold/" + d.number + ".png"})
       .attr("width", image_size + "px")
       .attr("height", image_size + "px")
-      .call(force.drag);
+      .call(force.drag)
+      .on('mousemove', (d) => handleMousemove(d))
+      .on('mouseout', (d) => removeTooltips());
 
     nodes.exit().remove();
 
@@ -137,3 +141,65 @@ d3.json("static/data/force_data_151.json", function(error, graph) {
   });
 
 });
+
+function handleMousemove(d) {
+  element = getIntersectingElements()[0];
+  if (element != undefined) {
+    data = element.__data__;
+    console.log('source, target:', data.source.name, data.target.name);
+    console.log('shared moves:', data.shared_moves);
+    makeTooltip(d, data);
+  }
+}
+
+function getIntersectingElements() {
+  var svg = d3.select('svg').node();
+  var irect = svg.createSVGRect();
+  var coordinates = d3.mouse(svg);
+  irect.x = coordinates[0];
+  irect.y = coordinates[1];
+  irect.width = irect.height = 1;
+  intersectingElements = [].slice.call(svg.getIntersectionList(irect, null));
+  intersectingLines = intersectingElements.filter((o) => o.nodeName == 'line');
+  actualLines = intersectingLines.filter((o) => {
+    coord = new Coord(irect.x, irect.y);
+    line = new Line(new Coord(o.x1.baseVal.value, o.y1.baseVal.value),
+                    new Coord(o.x2.baseVal.value, o.y2.baseVal.value));
+    return coordInRhombus(line, parseInt(o.style.strokeWidth), coord);
+  });
+  return actualLines;
+}
+
+function removeTooltips() {
+  body = d3.select('body');
+  body.selectAll('.tooltip').remove();
+}
+
+function makeTooltip(d, data) {
+  removeTooltips();
+
+  if (d.source == undefined) {
+    x = d.x;
+    y = d.y
+  } else {
+    x = (d.source.x + d.target.x) / 2;
+    y = (d.source.y + d.target.y) / 2;
+  }
+
+  svgContainer = d3.select('#svg_container');
+  tooltip = svgContainer.append('div')
+    .attr('class', 'tooltip')
+    .style('left', x + 70 + 'px')
+    .style('top', y + 70 + 'px');
+
+  tooltip.append('p')
+    .attr('class', 'heading')
+    .text(data.source.name + '-' + data.target.name);
+
+  data.shared_moves.map((o) => {
+    tooltip.append('p')
+      .attr('class', 'movename')
+      .text(o);
+  })
+
+}
